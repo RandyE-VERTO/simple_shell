a345466e87bccc0e92ef78c9f4e291c429c869e3
RandyE-VERTO <randyejejigbe@gmail.com>
2023-10-13 04:29:41 -0700

    Header files created

---
+++ shell.h
@@ -0,0 +1,15 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/type.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+
+
+int _putchar(char c);
+
+#endif
